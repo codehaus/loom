40cab4c0682f3e31bf56e340ccea85dfd22c2159
pdonald <pdonald@080c8e96-b372-0410-b4c6-c92817e0ab2a>
2003-06-26 03:18:16 +0000

    Re-add disable-proxy support

--- trunk/scm/loom/phoenix-client/src/java/org/apache/avalon/phoenix/metadata/BlockMetaData.java
+++ trunk/scm/loom/phoenix-client/src/java/org/apache/avalon/phoenix/metadata/BlockMetaData.java
@@ -18,14 +18,22 @@ public class BlockMetaData
 {
     private final String m_name;
     private final DependencyMetaData[] m_dependencies;
+
+    /**
+     * Add in boolean flag indicating whether this block
+     * should have proxy disabled.
+     */
+    private final boolean m_disableProxy;
     private final BlockInfo m_blockInfo;
 
     public BlockMetaData( final String name,
                           final DependencyMetaData[] dependencies,
+                          final boolean disableProxy,
                           final BlockInfo blockInfo )
     {
         m_name = name;
         m_dependencies = dependencies;
+        m_disableProxy = disableProxy;
         m_blockInfo = blockInfo;
     }
 
@@ -68,5 +76,15 @@ public class BlockMetaData
     public DependencyMetaData[] getDependencies()
     {
         return m_dependencies;
+    }
+
+    /**
+     * Return true if this block should not be accessed using a proxy.
+     *
+     * @return true if this block should not be accessed using a proxy.
+     */
+    public boolean isDisableProxy()
+    {
+        return m_disableProxy;
     }
 }
