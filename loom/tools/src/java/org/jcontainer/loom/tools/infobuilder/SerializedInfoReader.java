69fa65e30f8471af99355c7083e73b0243276717
pdonald <pdonald@080c8e96-b372-0410-b4c6-c92817e0ab2a>
2003-06-26 09:34:40 +0000

    import

---
+++ loom/tools/src/java/org/jcontainer/loom/tools/infobuilder/SerializedInfoReader.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) The JContainer Group. All rights reserved.
+ *
+ * This software is published under the terms of the JContainer
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.jcontainer.loom.tools.infobuilder;
+
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import org.jcontainer.loom.tools.info.ComponentInfo;
+import org.jcontainer.loom.tools.infobuilder.InfoReader;
+
+/**
+ * Create {@link org.jcontainer.loom.tools.info.ComponentInfo} objects from stream made up of
+ * serialized object.
+ *
+ * @author <a href="mailto:peter at apache.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2003-06-26 09:34:20 $
+ */
+public class SerializedInfoReader
+    implements InfoReader
+{
+    public ComponentInfo createComponentInfo( final String implementationKey,
+                                              final InputStream inputStream )
+        throws Exception
+    {
+        final ObjectInputStream ois = new ObjectInputStream( inputStream );
+        return (ComponentInfo)ois.readObject();
+    }
+}
