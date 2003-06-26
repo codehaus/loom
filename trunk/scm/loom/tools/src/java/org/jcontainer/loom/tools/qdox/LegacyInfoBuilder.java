9c99c4aa93fa87e6071b44953f3cbd75504a6e34
pdonald <pdonald@080c8e96-b372-0410-b4c6-c92817e0ab2a>
2003-06-26 08:46:40 +0000

    license

--- trunk/scm/loom/tools/src/java/org/jcontainer/loom/tools/qdox/LegacyInfoBuilder.java
+++ trunk/scm/loom/tools/src/java/org/jcontainer/loom/tools/qdox/LegacyInfoBuilder.java
@@ -4,6 +4,9 @@
  * This software is published under the terms of the JContainer
  * Software License version 1.1, a copy of which has been included
  * with this distribution in the LICENSE.txt file.
+ *
+ * This product includes software developed by the
+ * Apache Software Foundation (http://www.apache.org/).
  */
 package org.jcontainer.loom.tools.qdox;
 
@@ -28,7 +31,7 @@ import org.apache.avalon.phoenix.framework.tools.infobuilder.LegacyUtil;
  *
  * @author Paul Hammant
  * @author <a href="mailto:peter at apache.org">Peter Donald</a>
- * @version $Revision: 1.1 $ $Date: 2003-06-26 08:42:06 $
+ * @version $Revision: 1.2 $ $Date: 2003-06-26 08:46:40 $
  */
 public class LegacyInfoBuilder
     extends AbstractInfoBuilder
