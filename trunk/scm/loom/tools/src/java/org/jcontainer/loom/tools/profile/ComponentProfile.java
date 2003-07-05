360e4890bf8ed97a97cf4feb0881b2940d883657
pdonald <pdonald@080c8e96-b372-0410-b4c6-c92817e0ab2a>
2003-07-05 05:26:00 +0000

    Remove fqn in javadocs

--- trunk/scm/loom/tools/src/java/org/jcontainer/loom/tools/profile/ComponentProfile.java
+++ trunk/scm/loom/tools/src/java/org/jcontainer/loom/tools/profile/ComponentProfile.java
@@ -12,24 +12,24 @@ import org.jcontainer.loom.tools.metadata.ComponentMetaData;
 
 /**
  * The ComponentProfile defines a component as a conjunction
- * of the {@link org.jcontainer.loom.tools.info.ComponentInfo} and {@link org.jcontainer.loom.tools.metadata.ComponentMetaData}.
- * The {@link org.jcontainer.loom.tools.info.ComponentInfo} defines the type of the component
- * and the {@link org.jcontainer.loom.tools.metadata.ComponentMetaData} defines the data required to
+ * of the {@link ComponentInfo} and {@link ComponentMetaData}.
+ * The {@link ComponentInfo} defines the type of the component
+ * and the {@link ComponentMetaData} defines the data required to
  * construct a specific instance of the component.
  *
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.4 $ $Date: 2003-07-05 05:25:03 $
+ * @version $Revision: 1.5 $ $Date: 2003-07-05 05:26:00 $
  */
 public class ComponentProfile
 {
     /**
-     * The {@link org.jcontainer.loom.tools.info.ComponentInfo} that describes
+     * The {@link ComponentInfo} that describes
      * the type of this component.
      */
     private final ComponentInfo m_info;
 
     /**
-     * The {@link org.jcontainer.loom.tools.metadata.ComponentMetaData} that describes
+     * The {@link ComponentMetaData} that describes
      * this component.
      */
     private final ComponentMetaData m_metaData;
@@ -37,7 +37,7 @@ public class ComponentProfile
     /**
      * Creation of a new <code>ComponentProfile</code> instance.
      *
-     * @param metaData the {@link org.jcontainer.loom.tools.metadata.ComponentMetaData} instance defining the component.
+     * @param metaData the {@link ComponentMetaData} instance defining the component.
      */
     public ComponentProfile( final ComponentInfo info,
                              final ComponentMetaData metaData )
@@ -55,7 +55,7 @@ public class ComponentProfile
     }
 
     /**
-     * Returns the underlying {@link org.jcontainer.loom.tools.info.ComponentInfo} instance.
+     * Returns the underlying {@link ComponentInfo} instance.
      *
      * @return the component info instance
      */
@@ -65,7 +65,7 @@ public class ComponentProfile
     }
 
     /**
-     * Returns the underlying {@link org.jcontainer.loom.tools.metadata.ComponentMetaData} instance.
+     * Returns the underlying {@link ComponentMetaData} instance.
      * @return the component meta data instance
      */
     public ComponentMetaData getMetaData()
