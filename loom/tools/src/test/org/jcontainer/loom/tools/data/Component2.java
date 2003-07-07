c45b5eee4cf0287b5864773c57dfa34663d37672
pdonald <pdonald@080c8e96-b372-0410-b4c6-c92817e0ab2a>
2003-07-07 10:34:51 +0000

    Import some test data

---
+++ loom/tools/src/test/org/jcontainer/loom/tools/data/Component2.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) The JContainer Group. All rights reserved.
+ *
+ * This software is published under the terms of the JContainer
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.jcontainer.loom.tools.data;
+
+import org.jcontainer.loom.tools.data.Service2;
+
+/**
+ *
+ * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
+ * @version $Revision: 1.1 $ $Date: 2003-07-07 10:34:51 $
+ */
+public class Component2
+    implements Service2
+{
+}
