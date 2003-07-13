4451437def36467910e4d77a27a0c0677d174242
pdonald <pdonald@080c8e96-b372-0410-b4c6-c92817e0ab2a>
2003-07-13 06:00:39 +0000

    Use methods compatible with previous version of Mx4j

--- scm/loom/engine/src/java/org/jcontainer/loom/components/manager/MX4JSystemManager.java
+++ scm/loom/engine/src/java/org/jcontainer/loom/components/manager/MX4JSystemManager.java
@@ -223,9 +223,10 @@ public class MX4JSystemManager
         // Set the JNDI name with which will be registered
         mbean.setJNDIName( "jrmp" );
 
-        mbean.putJNDIProperty( javax.naming.Context.INITIAL_CONTEXT_FACTORY,
+        mbean.putNamingProperty( javax.naming.Context.INITIAL_CONTEXT_FACTORY,
                                m_namingFactory );
-        mbean.putJNDIProperty( javax.naming.Context.PROVIDER_URL, "rmi://localhost:" + m_rmi_registry_port );
+        mbean.putNamingProperty( javax.naming.Context.PROVIDER_URL,
+                                 "rmi://localhost:" + m_rmi_registry_port );
 
         // Register the JRMP adaptor in JNDI and start it
         mbean.start();
