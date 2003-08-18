0181cd73311be4bdf742e73fff961369368841a0
proyal <proyal@080c8e96-b372-0410-b4c6-c92817e0ab2a>
2003-08-18 02:14:23 +0000

    build scripts...

---
+++ trunk/loom/continuous-integration.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+mailto=jcontainer-interest@lists.codehaus.org
+builddir=.
+
+#JAVA_HOME=/usr/local/j2sdk1.4.1
+#MAVEN_HOME=~bwalding/maven
+#ANT_HOME=$HOME/cvs/ant/dist
+PATH=$HOME/bin:$JAVA_HOME/bin:$MAVEN_HOME/bin:$ANT_HOME/bin:$PATH
+#CVS_RSH=$HOME/ssh1.sh
+#CVSROOT=:ext:$USER@cvs.codehaus.org:/cvsroot/picocontainer
+
+export JAVA_HOME
+export MAVEN_HOME
+export ANT_HOME
+export PATH
+export CVS_RSH
+export CVSROOT
+
+cd $builddir
+
+# Clean old builds and make the target folder. Logs go here too.
+rm -Rf target
+mkdir target
+
+# Compile and test
+maven test:test &> target/cleanbuild.log
+
+# See if the "compiling" file is there. If it is, compilation
+# failed.
+if [ -e "target/compiling" ] ; then
+  # Mail Maven's output to the dev list.
+  cat target/cleanbuild.log | mutt -s "[BUILD] Clean build failed" $mailto
+else
+  # See if the "testfailure" file is there. If it is, tests failed.
+  if [ -e "target/testfailure" ] ; then
+    # Mail Maven's output to the dev list.
+    cat target/cleanbuild.log | mutt -s "[BUILD] Test failure - see http://loom.jcontainer.org/junit-report.html" $mailto
+  # else
+    # Deploy site only if compile and tests pass. Logs currently not used.
+    # Must be run separately to get the files uploaded in the proper dir
+    # on the server.
+    # maven jar:deploy &> target/jardeploy.log
+    # maven dist:deploy &> target/distdeploy.log
+  fi
+  # We'll deploy the site even if the tests fail. Log currently not used.
+  # maven site:deploy &> target/sitedeploy.log
+fi
