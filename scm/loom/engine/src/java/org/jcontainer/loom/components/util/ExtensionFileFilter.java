ff97720f2b27d60b16f75880a63ba690d9342a4a
pdonald <pdonald@080c8e96-b372-0410-b4c6-c92817e0ab2a>
2003-10-05 00:31:36 +0000

    Correct license

--- scm/loom/engine/src/java/org/jcontainer/loom/components/util/ExtensionFileFilter.java
+++ scm/loom/engine/src/java/org/jcontainer/loom/components/util/ExtensionFileFilter.java
@@ -1,7 +1,7 @@
 /*
- * Copyright (C) The Spice Group. All rights reserved.
+ * Copyright (C) The JContainer Group. All rights reserved.
  *
- * This software is published under the terms of the Spice
+ * This software is published under the terms of the JContainer
  * Software License version 1.1, a copy of which has been included
  * with this distribution in the LICENSE.txt file.
  */
@@ -13,7 +13,7 @@ import java.io.FilenameFilter;
 /**
  *
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.1 $ $Date: 2003-10-05 00:30:20 $
+ * @version $Revision: 1.2 $ $Date: 2003-10-05 00:31:36 $
  */
 public class ExtensionFileFilter
     implements FilenameFilter
