943637fed0d3fcb6ec1498f33ce52def76777e5c
pdonald <pdonald@080c8e96-b372-0410-b4c6-c92817e0ab2a>
2003-10-05 01:21:55 +0000

    Remove duplication and add warning about deprecated task

--- trunk/loom/tools/src/java/org/jcontainer/loom/tools/tasks/Sar.java
+++ trunk/loom/tools/src/java/org/jcontainer/loom/tools/tasks/Sar.java
@@ -7,10 +7,8 @@
  */
 package org.jcontainer.loom.tools.tasks;
 
-import java.io.File;
+import org.jcontainer.loom.tools.ant.SarTask;
 import org.apache.tools.ant.BuildException;
-import org.apache.tools.ant.taskdefs.Jar;
-import org.apache.tools.ant.types.ZipFileSet;
 
 /**
  * Creates a Sar archive.
@@ -19,139 +17,12 @@ import org.apache.tools.ant.types.ZipFileSet;
  * @author <a href="mailto:stefan.bodewig@epost.de">Stefan Bodewig</a>
  */
 public class Sar
-    extends Jar
+    extends SarTask
 {
-    private File m_config;
-    private File m_assembly;
-    private File m_environment;
-
-    public Sar()
-    {
-        archiveType = "sar";
-        emptyBehavior = "fail";
-    }
-
-    public void setSarfile( final File file )
-    {
-        setDestFile( file );
-    }
-
-    public void setConfig( final File config )
-    {
-        m_config = config;
-
-        if( !m_config.exists() )
-        {
-            final String message =
-                "Config descriptor: " + m_config + " does not exist.";
-            throw new BuildException( message, getLocation() );
-        }
-
-        if( !m_config.isFile() )
-        {
-            final String message =
-                "Config descriptor: " + m_config + " is not a file.";
-            throw new BuildException( message, getLocation() );
-        }
-    }
-
-    public void setAssembly( final File assembly )
-    {
-        m_assembly = assembly;
-
-        if( !m_assembly.exists() )
-        {
-            final String message =
-                "Assembly descriptor: " + m_assembly + " does not exist.";
-            throw new BuildException( message, getLocation() );
-        }
-
-        if( !m_assembly.isFile() )
-        {
-            final String message =
-                "Assembly descriptor: " + m_assembly + " is not a file.";
-            throw new BuildException( message, getLocation() );
-        }
-    }
-
-    public void setServer( final File server )
-    {
-        System.err.println( "DEPRECATED: Server attribute of sar task is deprecated" );
-        setEnvironment( server );
-    }
-
-    public void setEnvironment( final File environment )
-    {
-        m_environment = environment;
-
-        if( !m_environment.exists() )
-        {
-            final String message = "Environment descriptor: " +
-                m_environment + " does not exist.";
-            throw new BuildException( message, getLocation() );
-        }
-
-        if( !m_environment.isFile() )
-        {
-            final String message = "Environment descriptor: " +
-                m_environment + " is not a file.";
-            throw new BuildException( message, getLocation() );
-        }
-    }
-
-    public void addLib( final ZipFileSet zipFileSet )
-    {
-        zipFileSet.setPrefix( "SAR-INF/lib" );
-        super.addFileset( zipFileSet );
-    }
-
-    public void addClasses( final ZipFileSet zipFileSet )
-    {
-        zipFileSet.setPrefix( "SAR-INF/classes" );
-        super.addFileset( zipFileSet );
-    }
-
     public void execute()
         throws BuildException
     {
-        if( null == m_config )
-        {
-            final String message = "config attribute is required";
-            throw new BuildException( message, getLocation() );
-        }
-        if( null == m_assembly )
-        {
-            final String message = "assembly attribute is required";
-            throw new BuildException( message, getLocation() );
-        }
-        if( null == m_environment )
-        {
-            final String message = "environment attribute is required";
-            throw new BuildException( message, getLocation() );
-        }
-
-        pushFile( "SAR-INF/config.xml", m_config );
-        pushFile( "SAR-INF/assembly.xml", m_assembly );
-        pushFile( "SAR-INF/environment.xml", m_environment );
-
+        log( "Sar task has been replaced with task defined by " + SarTask.class.getName() );
         super.execute();
-    }
-
-    private void pushFile( final String path, final File file )
-    {
-        final ZipFileSet zipFileSet = new ZipFileSet();
-        zipFileSet.setDir( new File( file.getParent() ) );
-        zipFileSet.setIncludes( file.getName() );
-        zipFileSet.setFullpath( path );
-        super.addFileset( zipFileSet );
-    }
-
-    protected void cleanUp()
-    {
-        super.cleanUp();
-
-        m_config = null;
-        m_assembly = null;
-        m_environment = null;
     }
 }
