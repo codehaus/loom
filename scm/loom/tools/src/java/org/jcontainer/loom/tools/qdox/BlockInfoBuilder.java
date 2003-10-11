450bed956ab1c226f84a7d4eec283da225bfc772
pdonald <pdonald@080c8e96-b372-0410-b4c6-c92817e0ab2a>
2003-10-11 08:59:20 +0000

    Remove uneeded base class

--- scm/loom/tools/src/java/org/jcontainer/loom/tools/qdox/BlockInfoBuilder.java
+++ scm/loom/tools/src/java/org/jcontainer/loom/tools/qdox/BlockInfoBuilder.java
@@ -89,6 +89,7 @@ package org.jcontainer.loom.tools.qdox;
 import com.thoughtworks.qdox.model.DocletTag;
 import com.thoughtworks.qdox.model.JavaClass;
 import com.thoughtworks.qdox.model.JavaMethod;
+import com.thoughtworks.qdox.model.Type;
 import java.util.ArrayList;
 import org.jcontainer.loom.tools.info.ComponentInfo;
 import org.jcontainer.loom.tools.info.DependencyDescriptor;
@@ -103,11 +104,17 @@ import org.realityforge.metaclass.model.Attribute;
  *
  * @author Paul Hammant
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.1 $ $Date: 2003-10-11 08:44:59 $
+ * @version $Revision: 1.2 $ $Date: 2003-10-11 08:59:20 $
  */
 public class BlockInfoBuilder
-    extends AbstractInfoBuilder
 {
+    private static final String COMPONENT_MANAGER_CLASS =
+        "org.apache.avalon.framework.component.ComponentManager";
+    private static final String SERVICE_MANAGER_CLASS =
+        "org.apache.avalon.framework.service.ServiceManager";
+    private static final String CONFIGURATION_CLASS =
+        "org.apache.avalon.framework.configuration.Configuration";
+
     /**
      * Build a ComponentInfo object for specified class.
      *
@@ -219,5 +226,98 @@ public class BlockInfoBuilder
             }
             return (DependencyDescriptor[])deps.toArray( new DependencyDescriptor[ deps.size() ] );
         }
+    }
+
+    /**
+     * Resolve the specified type.
+     * Resolving essentially means finding the fully qualified name of
+     * a class from just it's short name.
+     *
+     * @param javaClass the java class relative to which the type must be resolved
+     * @param type the unresolved type
+     * @return the resolved type
+     */
+    private String resolveType( final JavaClass javaClass,
+                                final String type )
+    {
+        return javaClass.getParentSource().resolveType( type );
+    }
+
+    /**
+     * Retrieve a method with specified name and one parameter of specified
+     * type. The method must also return void.
+     *
+     * @param javaClass the java class to retrieve method for
+     * @param methodName the name of the method
+     * @param parameterType the class name of parameter
+     * @return the method if such a method exists
+     */
+    private JavaMethod getLifecycleMethod( final JavaClass javaClass,
+                                           final String methodName,
+                                           final String parameterType )
+    {
+        final JavaMethod[] methods = javaClass.getMethods();
+        for( int i = 0; i < methods.length; i++ )
+        {
+            final JavaMethod method = methods[ i ];
+            if( methodName.equals( method.getName() ) &&
+                method.getReturns().equals( new Type( "void", 0 ) ) &&
+                method.getParameters().length == 1 &&
+                method.getParameters()[ 0 ].getType().getValue().equals( parameterType ) )
+            {
+                return method;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Retrieve specified named parameter from tag. If the parameter
+     * does not exist then return specified default value.
+     *
+     * @param tag the tag
+     * @param name the name of parameter
+     * @return the value of named parameter
+     */
+    private String getNamedParameter( final DocletTag tag,
+                                      final String name,
+                                      final String defaultValue )
+    {
+        String value = tag.getNamedParameter( name );
+        if( null == value )
+        {
+            return defaultValue;
+        }
+        value = value.trim();
+        if( value.startsWith( "\"" ) || value.startsWith( "'" ) )
+        {
+            value = value.substring( 1 );
+        }
+        if( value.endsWith( "\"" ) || value.endsWith( "'" ) )
+        {
+            value = value.substring( 0, value.length() - 1 );
+        }
+        return value;
+    }
+
+    /**
+     * Retrieve specified named parameter from tag. If the parameter
+     * does not exist then throw an exception.
+     *
+     * @param tag the tag
+     * @param name the name of parameter
+     * @return the value of named parameter
+     */
+    private String getNamedParameter( final DocletTag tag, final String name )
+    {
+        final String value = getNamedParameter( tag, name, null );
+        if( null == value )
+        {
+            final String message =
+                "Malformed tag '" + tag.getName() + "'. " +
+                "Missing required parameter '" + name + "'";
+            throw new IllegalArgumentException( message );
+        }
+        return value;
     }
 }
