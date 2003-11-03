a8038b6d9b3fb0263a2e28e1936d423baabd33af
pdonald <pdonald@080c8e96-b372-0410-b4c6-c92817e0ab2a>
2003-11-03 06:19:27 +0000

    Remove fqn

--- trunk/loom/engine/src/java/org/jcontainer/loom/components/util/profile/ComponentProfile.java
+++ trunk/loom/engine/src/java/org/jcontainer/loom/components/util/profile/ComponentProfile.java
@@ -12,13 +12,13 @@ import org.jcontainer.loom.components.util.metadata.ComponentMetaData;
 
 /**
  * The ComponentProfile defines a component as a conjunction
- * of the {@link org.jcontainer.loom.components.util.info.ComponentInfo} and {@link org.jcontainer.loom.components.util.metadata.ComponentMetaData}.
- * The {@link org.jcontainer.loom.components.util.info.ComponentInfo} defines the type of the component
- * and the {@link org.jcontainer.loom.components.util.metadata.ComponentMetaData} defines the data required to
+ * of the ComponentInfo and ComponentMetaData.
+ * The ComponentInfo defines the type of the component
+ * and the ComponentMetaData defines the data required to
  * construct a specific instance of the component.
  *
  * @author <a href="mailto:peter at realityforge.org">Peter Donald</a>
- * @version $Revision: 1.1 $ $Date: 2003-10-16 14:45:47 $
+ * @version $Revision: 1.2 $ $Date: 2003-11-03 06:19:27 $
  */
 public class ComponentProfile
 {
@@ -28,13 +28,13 @@ public class ComponentProfile
     public static final ComponentProfile[] EMPTY_SET = new ComponentProfile[ 0 ];
 
     /**
-     * The {@link org.jcontainer.loom.components.util.info.ComponentInfo} that describes
+     * The ComponentInfo that describes
      * the type of this component.
      */
     private final ComponentInfo m_info;
 
     /**
-     * The {@link org.jcontainer.loom.components.util.metadata.ComponentMetaData} that describes
+     * The ComponentMetaData that describes
      * this component.
      */
     private final ComponentMetaData m_metaData;
@@ -42,7 +42,7 @@ public class ComponentProfile
     /**
      * Creation of a new <code>ComponentProfile</code> instance.
      *
-     * @param metaData the {@link org.jcontainer.loom.components.util.metadata.ComponentMetaData} instance defining the component.
+     * @param metaData the ComponentMetaData instance defining the component.
      */
     public ComponentProfile( final ComponentInfo info,
                              final ComponentMetaData metaData )
@@ -60,7 +60,7 @@ public class ComponentProfile
     }
 
     /**
-     * Returns the underlying {@link org.jcontainer.loom.components.util.info.ComponentInfo} instance.
+     * Returns the underlying ComponentInfo instance.
      *
      * @return the component info instance
      */
@@ -70,7 +70,7 @@ public class ComponentProfile
     }
 
     /**
-     * Returns the underlying {@link org.jcontainer.loom.components.util.metadata.ComponentMetaData} instance.
+     * Returns the underlying ComponentMetaData instance.
      * @return the component meta data instance
      */
     public ComponentMetaData getMetaData()
