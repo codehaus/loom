cf09ea9dae3c7fbef9e26f7dc58ad1d750701bc4
mauro <mauro@080c8e96-b372-0410-b4c6-c92817e0ab2a>
2003-12-02 16:52:30 +0000

    Added test data and test dependency

---
+++ trunk/scm/loom/plugins/maven-sar/src/plugin-test/src/java/Test.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) The JContainer Group. All rights reserved.
+ *
+ * This software is published under the terms of the JContainer
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+
+/**
+ * This is a Test class
+ * 
+ */
+public class Test
+{
+}
