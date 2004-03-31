de991ffb6c7fa2eff1a6baa7a59f3635c7a3a7d3
pdonald <pdonald@080c8e96-b372-0410-b4c6-c92817e0ab2a>
2004-03-31 03:18:56 +0000

    Migrate to new package names

---
+++ trunk/loom/support/classman/src/testdata/org/jcontainer/loom/classman/test/data/cl1/Data1.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) The Spice Group. All rights reserved.
+ *
+ * This software is published under the terms of the Spice
+ * Software License version 1.1, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+package org.jcontainer.loom.classman.test.data.cl1;
+
+/**
+ *
+ * @author Peter Donald
+ * @version $Revision: 1.1 $ $Date: 2004-03-31 03:18:56 $
+ */
+public class Data1
+{
+}
