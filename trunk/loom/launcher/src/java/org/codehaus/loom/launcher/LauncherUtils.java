a2db3b55df6e1fd9bb8f8a21f6954c1c31b2edcf
mauro <mauro@080c8e96-b372-0410-b4c6-c92817e0ab2a>
2004-04-19 21:41:17 +0000

    Renamed org.jcontainer.loom -> org.codehaus.loom

---
+++ trunk/loom/launcher/src/java/org/codehaus/loom/launcher/LauncherUtils.java
@@ -0,0 +1,201 @@
+/* ====================================================================
+ * JContainer Software License, version 1.1
+ *
+ * Copyright (c) 2003, JContainer Group. All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Neither the name of the JContainer Group nor the name "Loom" nor
+ *    the names of its contributors may be used to endorse or promote
+ *    products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ *
+ * JContainer Loom includes code from the Apache Software Foundation
+ *
+ * ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1997-2003 The Apache Software Foundation. All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *    "This product includes software developed by the
+ *    Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software
+ *    itself, if and wherever such third-party acknowledgments
+ *    normally appear.
+ *
+ * 4. The names "Jakarta", "Avalon", and "Apache Software Foundation"
+ *    must not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+package org.codehaus.loom.launcher;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/**
+ * A set of utilities that help when writing Launchers.
+ *
+ * @author Peter Donald
+ */
+public class LauncherUtils
+{
+    /**
+     * Create a ClassPath for the engine.
+     *
+     * @return the set of URLs that engine uses to load
+     * @throws Exception if unable to aquire classpath
+     */
+    static URL[] generateClassPath( final File homeDir,
+                                    final String libDirectory )
+        throws Exception
+    {
+        final ArrayList urls = new ArrayList();
+
+        final File dir = findLibDir( homeDir, libDirectory );
+        final File[] files = dir.listFiles();
+        for( int i = 0; i < files.length; i++ )
+        {
+            final File file = files[ i ];
+            if( file.getName().endsWith( ".jar" ) )
+            {
+                urls.add( file.toURL() );
+            }
+        }
+
+        return (URL[])urls.toArray( new URL[ urls.size() ] );
+    }
+
+    /**
+     * Find directory to load engine specific libraries from.
+     *
+     * @return the lib dir
+     * @throws Exception if unable to aquire directory
+     */
+    static File findLibDir( final File homeDir,
+                            final String libDirectory )
+        throws Exception
+    {
+        final String engineLibDir = libDirectory.replace( '/',
+                                                          File.separatorChar );
+        final File dir = new File( homeDir, engineLibDir ).getCanonicalFile();
+        if( !dir.exists() )
+        {
+            throw new Exception(
+                "Unable to locate library directory at " + engineLibDir );
+        }
+        return dir;
+    }
+
+    /**
+     * Utility method to find the home directory of Loom and make sure system
+     * property is set to it.
+     *
+     * @return the location of loom directory
+     * @throws Exception if unable to locate directory
+     */
+    static File findLoomHome( final String systemProperty,
+                              final String loaderJar )
+        throws Exception
+    {
+        String loomHome = System.getProperty( systemProperty, null );
+        if( null == loomHome )
+        {
+            final File loaderDir =
+                findLoaderDir( systemProperty, loaderJar );
+            return loaderDir.getCanonicalFile().getParentFile();
+        }
+        else
+        {
+            return new File( loomHome ).getCanonicalFile();
+        }
+    }
+
+    /**
+     * Finds the loaderJar file in the classpath.
+     */
+    private static final File findLoaderDir( final String systemProperty,
+                                             final String loaderJar )
+        throws Exception
+    {
+        final String classpath = System.getProperty( "java.class.path" );
+        final String pathSeparator = System.getProperty( "path.separator" );
+        final StringTokenizer tokenizer = new StringTokenizer( classpath,
+                                                               pathSeparator );
+
+        while( tokenizer.hasMoreTokens() )
+        {
+            final String element = tokenizer.nextToken();
+
+            if( element.endsWith( loaderJar ) )
+            {
+                File file = ( new File( element ) ).getCanonicalFile();
+                file = file.getParentFile();
+                return file;
+            }
+        }
+
+        throw new Exception( "Unable to locate " +
+                             loaderJar +
+                             " in classpath. User must specify " +
+                             systemProperty + " system property." );
+    }
+}
---
+++ trunk/scm/loom/demos/avalonlifecycle/src/java/org/codehaus/loom/demos/avalonlifecycle/AlternativeLifecycle1Impl.java
@@ -0,0 +1,105 @@
+/* ====================================================================
+ * JContainer Software License, version 1.1
+ *
+ * Copyright (c) 2003, JContainer Group. All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. Neither the name of the JContainer Group nor the name "Loom" nor
+ *    the names of its contributors may be used to endorse or promote
+ *    products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ *
+ * JContainer Loom includes code from the Apache Software Foundation
+ *
+ * ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1997-2003 The Apache Software Foundation. All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *    "This product includes software developed by the
+ *    Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software
+ *    itself, if and wherever such third-party acknowledgments
+ *    normally appear.
+ *
+ * 4. The names "Jakarta", "Avalon", and "Apache Software Foundation"
+ *    must not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+package org.codehaus.loom.demos.avalonlifecycle;
+
+/**
+ * A demo of the lifecycle methods.  Mount the SAR fle contaning there blocks in
+ * Loom, go to the JMX console .. http://localhost:8082/mbean?objectname=Loom%3Aapplication%3Ddemo-avalonlifecycle%2Ctopic%3DApplication
+ * .. and try stopt/starting the blocks.
+ *
+ * @author Paul Hammant <Paul_Hammant@yahoo.com>
+ * @dna.component
+ * @dna.service type="Lifecycle1"
+ */
+public class AlternativeLifecycle1Impl
+    implements Lifecycle1
+{
+    public int myServiceMethod()
+    {
+        return 456;
+    }
+}
